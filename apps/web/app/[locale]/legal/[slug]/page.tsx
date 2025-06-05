35157a67adcba967a9bb4533fab87f783a8a57f8
Swaraj Bachu <rajswaraj.r@gmail.com>
2025-06-05 17:45:04 +0530

    Refactor content collections and add utility function for text truncation

---
+++ apps/web/app/[locale]/legal/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import { BlogLayout } from '@/components/blog/blog-layout';
+import CTA from '@/components/cta';
+import { components } from '@/components/mdx-components';
+import { MDXContent } from '@content-collections/mdx/react';
+import { allLegals } from 'content-collections';
+import { notFound } from 'next/navigation';
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const legal = allLegals.find((l) => l.slug === slug);
+
+  console.log(allLegals, 'all legals');
+  console.log(slug, 'slug');
+  if (!legal) {
+    return notFound();
+  }
+
+  const legalWithType = {
+    ...legal,
+    type: 'legal' as const,
+  };
+
+  return (
+    <div>
+      <BlogLayout blog={legalWithType}>
+        <MDXContent components={components} code={legal.body} />
+      </BlogLayout>
+      <div className="py-20">
+        <CTA />
+      </div>
+    </div>
+  );
+}
--- apps/web/components/blog/blog-card.tsx
+++ apps/web/components/blog/blog-card.tsx
@@ -1,65 +1,64 @@
-import React from "react";
-import { BlurImage } from "@/components/blog/blur-image";
-import Image from "next/image";
-import Balancer from "react-wrap-balancer";
-import { truncate } from "@/lib/utils";
-import { format } from "date-fns";
-import { Blog } from "content-collections";
-import { Logo } from "../logo";
-import Link from "next/link";
+import { BlurImage } from '@/components/blog/blur-image';
+import { truncate } from '@/lib/utils';
+import type { Blog } from 'content-collections';
+import { format } from 'date-fns';
+import Image from 'next/image';
+import Link from 'next/link';
+import Balancer from 'react-wrap-balancer';
+import { Logo } from '../logo';
 
 export const BlogCard = ({ blog }: { blog: Blog }) => {
   return (
     <Link
-      className="shadow-lg grid grid-cols-1 md:grid-cols-2 rounded-3xl group border border-transparent hover:border-gray-200 w-full hover:bg-gray-50 overflow-hidden hover:scale-[1.02] transition duration-200"
+      className="group grid w-full grid-cols-1 overflow-hidden rounded-3xl border border-transparent shadow-lg transition duration-200 hover:scale-[1.02] hover:border-gray-200 hover:bg-gray-50 md:grid-cols-2"
       href={`/blog/${blog.slug}`}
     >
       <div className="">
         {blog.image ? (
           <BlurImage
-            src={blog.image || ""}
+            src={blog.image || ''}
             alt={blog.title}
             height="800"
             width="800"
-            className="h-full max-h-96 object-cover object-top w-full rounded-3xl"
+            className="h-full max-h-96 w-full rounded-3xl object-cover object-top"
           />
         ) : (
-          <div className="h-full flex items-center justify-center group-hover:bg-gray-50">
+          <div className="flex h-full items-center justify-center group-hover:bg-gray-50">
             <Logo />
           </div>
         )}
       </div>
-      <div className="p-4 md:p-8 group-hover:bg-gray-50 flex flex-col justify-between">
+      <div className="flex flex-col justify-between p-4 group-hover:bg-gray-50 md:p-8">
         <div>
-          <div className="flex gap-4 flex-wrap mb-4">
+          <div className="mb-4 flex flex-wrap gap-4">
             {blog.categories?.map((category, idx) => (
               <p
                 key={`category-${idx}`}
-                className="text-xs font-bold text-gray-600 px-4 py-2 rounded-full bg-gray-100 capitalize"
+                className="rounded-full bg-gray-100 px-4 py-2 font-bold text-gray-600 text-xs capitalize"
               >
                 {category}
               </p>
             ))}
           </div>
-          <p className="text-lg md:text-4xl font-bold mb-4 text-gray-900">
+          <p className="mb-4 font-bold text-gray-900 text-lg md:text-4xl">
             <Balancer>{blog.title}</Balancer>
           </p>
-          <p className="text-left text-base md:text-xl mt-2 text-gray-600">
+          <p className="mt-2 text-left text-base text-gray-600 md:text-xl">
             {truncate(blog.description, 500)}
           </p>
         </div>
-        <div className="flex space-x-2 items-center mt-6">
+        <div className="mt-6 flex items-center space-x-2">
           <Image
             src={blog.authorAvatar}
             alt={blog.author}
             width={20}
             height={20}
-            className="rounded-full h-5 w-5"
+            className="h-5 w-5 rounded-full"
           />
-          <p className="text-sm font-normal text-gray-600">{blog.author}</p>
-          <div className="h-1 w-1 bg-gray-400 rounded-full"></div>
-          <p className="text-gray-600 text-sm max-w-xl group-hover:text-gray-900 transition duration-200">
-            {format(new Date(blog.date), "MMMM dd, yyyy")}
+          <p className="font-normal text-gray-600 text-sm">{blog.author}</p>
+          <div className="h-1 w-1 rounded-full bg-gray-400" />
+          <p className="max-w-xl text-gray-600 text-sm transition duration-200 group-hover:text-gray-900">
+            {format(new Date(blog.date), 'MMMM dd, yyyy')}
           </p>
         </div>
       </div>
@@ -68,57 +67,57 @@ export const BlogCard = ({ blog }: { blog: Blog }) => {
 };
 
 export const BlogCardVertical = ({ blog }: { blog: Blog }) => {
-  return (
+  return (  
     <Link
-      className="shadow-lg rounded-3xl group border border-transparent hover:border-gray-200 w-full hover:bg-gray-50 overflow-hidden hover:scale-[1.02] transition duration-200"
+      className="group w-full overflow-hidden rounded-3xl border border-transparent shadow-lg transition duration-200 hover:scale-[1.02] hover:border-gray-200 hover:bg-gray-50"
       href={`${blog.slug}`}
     >
       <div className="">
         {blog.image ? (
           <BlurImage
-            src={blog.image || ""}
+            src={blog.image || ''}
             alt={blog.title}
             height="800"
             width="800"
-            className="h-64 md:h-96 object-cover object-top w-full rounded-3xl"
+            className="h-64 w-full rounded-3xl object-cover object-top md:h-96"
           />
         ) : (
-          <div className="h-64 md:h-96 flex items-center justify-center group-hover:bg-gray-50">
+          <div className="flex h-64 items-center justify-center group-hover:bg-gray-50 md:h-96">
             <Logo />
           </div>
         )}
       </div>
-      <div className="p-4 md:p-8 group-hover:bg-gray-50 flex flex-col justify-between">
+      <div className="flex flex-col justify-between p-4 group-hover:bg-gray-50 md:p-8">
         <div>
-          <div className="flex gap-4 flex-wrap mb-4">
+          <div className="mb-4 flex flex-wrap gap-4">
             {blog.categories?.map((category, idx) => (
               <p
                 key={`category-${idx}`}
-                className="text-xs font-bold text-gray-600 px-4 py-2 rounded-full bg-gray-100 capitalize"
+                className="rounded-full bg-gray-100 px-4 py-2 font-bold text-gray-600 text-xs capitalize"
               >
                 {category}
               </p>
             ))}
           </div>
-          <p className="text-lg md:text-xl font-bold mb-4 text-gray-900">
+          <p className="mb-4 font-bold text-gray-900 text-lg md:text-xl">
             <Balancer>{blog.title}</Balancer>
           </p>
-          <p className="text-left text-sm md:text-base mt-2 text-gray-600">
+          <p className="mt-2 text-left text-gray-600 text-sm md:text-base">
             {truncate(blog.description, 200)}
           </p>
         </div>
-        <div className="flex space-x-2 items-center mt-6">
+        <div className="mt-6 flex items-center space-x-2">
           <Image
             src={blog.authorAvatar}
             alt={blog.author}
             width={20}
             height={20}
-            className="rounded-full h-5 w-5"
+            className="h-5 w-5 rounded-full"
           />
-          <p className="text-sm font-normal text-gray-600">{blog.author}</p>
-          <div className="h-1 w-1 bg-gray-400 rounded-full"></div>
-          <p className="text-gray-600 text-sm max-w-xl group-hover:text-gray-900 transition duration-200">
-            {format(new Date(blog.date), "MMMM dd, yyyy")}
+          <p className="font-normal text-gray-600 text-sm">{blog.author}</p>
+          <div className="h-1 w-1 rounded-full bg-gray-400" />
+          <p className="max-w-xl text-gray-600 text-sm transition duration-200 group-hover:text-gray-900">
+            {format(new Date(blog.date), 'MMMM dd, yyyy')}
           </p>
         </div>
       </div>
