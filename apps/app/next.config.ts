a371970c6c1000cb785ee3c009c1a2d46edf491b
Swaraj Bachu <rajswaraj.r@gmail.com>
2025-06-27 20:35:01 +0530

    Refactor Next.js configuration: remove Sentry integration, streamline imports, and add patch for @react-email/render

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,14 +1,10 @@
 import { config, withAnalyzer } from '@delulu/next-config';
-import { withLogging, withSentry } from '@delulu/observability/next-config';
+import { withLogging } from '@delulu/observability/next-config';
 import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 import { env } from 'env';
 import type { NextConfig } from 'next';
 
 let nextConfig: NextConfig = withLogging(config);
-
-if (env.VERCEL) {
-  nextConfig = withSentry(nextConfig);
-}
 
 if (env.ANALYZE === 'true') {
   nextConfig = withAnalyzer(nextConfig);
--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,7 +1,5 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
-// @ts-expect-error No declaration file
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 import type { NextConfig } from 'next';
 
 const otelRegex = /@opentelemetry\/instrumentation/;
@@ -40,7 +38,6 @@ export const config: NextConfig = {
   webpack(config, { isServer }) {
     if (isServer) {
       config.plugins = config.plugins || [];
-      config.plugins.push(new PrismaPlugin());
     }
 
     config.ignoreWarnings = [{ module: otelRegex }];
