17b99f2268fc93a02024ae6ccc4133544639f15b
Swaraj Bachu <rajswaraj.r@gmail.com>
2025-10-05 01:32:26 +0530

    refactor: update scroll area component and enhance touch support
    - Updated the ScrollArea component to include touch support and improved mask functionality for better user experience on touch devices.
    - Refactored sidebar styles for improved clarity and responsiveness.
    - Updated package dependencies for '@radix-ui/react-scroll-area' to version 1.2.9 and added 'tw-animate-css' back to package.json with version 1.3.4.

--- apps/app/components/layout/sidebar.tsx
+++ apps/app/components/layout/sidebar.tsx
@@ -140,7 +140,7 @@ export const GlobalSidebar = ({ children }: GlobalSidebarProperties) => {
                   asChild
                   size="lg"
                   className={cn(
-                    'group ease relative mb-1 select-none rounded-md px-3 py-2.5 transition-colors duration-100 hover:bg-accent hover:text-primary-foreground ',
+                    'group/menu ease relative mb-1 select-none rounded-md px-3 py-2.5 transition-colors duration-100 hover:bg-accent hover:text-primary-foreground ',
                     pathname === item.url &&
                       "bg-[#F9F9FA] shadow-[0px_11px_4px_rgba(7,7,8,0.01),0px_6px_4px_rgba(7,7,8,0.02),0px_3px_3px_rgba(7,7,8,0.04),0px_1px_1px_rgba(7,7,8,0.05)] before:absolute before:inset-0 before:rounded-md before:border before:border-white/5 before:content-[''] after:absolute after:inset-0 after:rounded-md after:bg-[radial-gradient(at_top,rgba(255,255,255,0.05)_5%,rgba(255,255,255,0)_100%)] after:shadow-[inset_0px_-2px_0px_0px_rgba(7,7,8,0.06)] after:content-[''] dark:bg-[#2A2A2A] dark:shadow-md dark:after:shadow-[inset_0px_-2px_0px_0px_rgba(7,7,8,0.3)]",
                     'group-data-[state=collapsed]:justify-center'
@@ -164,13 +164,13 @@ export const GlobalSidebar = ({ children }: GlobalSidebarProperties) => {
                         'h-5 w-5',
                         pathname === item.url
                           ? 'text-foreground'
-                          : 'text-muted-foreground group-hover:text-foreground'
+                          : 'text-muted-foreground group-hover/menu:text-foreground'
                       )}
                     />
                     <span
                       className={cn(
                         'ml-3 font-medium text-muted-foreground text-sm',
-                        'group-data-[state=collapsed]:hidden',
+                        'group-hover/menu:text-foreground group-data-[state=collapsed]:hidden',
                         pathname === item.url && 'text-foreground'
                       )}
                     >
